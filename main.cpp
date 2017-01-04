107cff55b68c65f3c28b3dfb2efd1979b042524a
WiseSoul <silviu.patras@gmail.com>
2017-01-04 23:32:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
 #include <time.h>
 
 using namespace std;
@@ -179,4 +179,148 @@ void player_move(bool isPlayerOne)
     matrix[x][y] = sign;
 }
 
+void AI_move_Easy(void)
+{
+	int i, j;
+	for (i = 0; i<3; i++) {
+		for (j = 0; j<3; j++)
+			if (matrix[i][j] == ' ') break;
+		if (matrix[i][j] == ' ') break;
+	}
 
+	if (i*j == 9)
+		cout << "draw\n";
+	else
+		matrix[i][j] = playerTwoSymbol;
+}
+
+
+void disp_matrix(void) //matrix display
+{
+	int t;
+    cout << endl;
+
+	for (t = 0; t<3; t++)
+	{
+
+		cout << " " << char(matrix[t][0]) << " | " << char(matrix[t][1]) << " | " << char(matrix[t][2]);
+
+		if (t != 2)
+			cout << "\n---|---|---\n";
+	}
+	cout << endl << endl;
+}
+
+char check(void) //used for identifying winner
+{
+	int i;
+
+	for (i = 0; i<3; i++)  /* check rows */
+		if (matrix[i][0] == matrix[i][1] &&
+			matrix[i][0] == matrix[i][2]) return matrix[i][0];
+
+	for (i = 0; i<3; i++)  /* check columns */
+		if (matrix[0][i] == matrix[1][i] &&
+			matrix[0][i] == matrix[2][i]) return matrix[0][i];
+
+	/* test diagonals */
+	if (matrix[0][0] == matrix[1][1] &&
+		matrix[1][1] == matrix[2][2])
+		return matrix[0][0];
+
+	if (matrix[0][2] == matrix[1][1] &&
+		matrix[1][1] == matrix[2][0])
+		return matrix[0][2];
+
+	return ' ';
+}
+
+
+bool castigator(int mat[3][3])
+{
+	char a, b, c, d, e, f, g, h, i;
+	a = mat[0][0], b = mat[0][1], c = mat[0][2];
+	d = mat[1][0], e = mat[1][1], f = mat[1][2];
+	g = mat[2][0], h = mat[2][1], i = mat[2][2];
+	if ((a == playerTwoSymbol && b == playerTwoSymbol && c != playerOneSymbol) || (b == playerTwoSymbol && c == playerTwoSymbol && a != playerOneSymbol) || (a == playerTwoSymbol && c == playerTwoSymbol && b != playerOneSymbol))
+	{
+		row = 1;
+		return true;
+	}
+	if ((d == playerTwoSymbol && e == playerTwoSymbol && f != playerOneSymbol) || (e == playerTwoSymbol && f == playerTwoSymbol && d != playerOneSymbol) || (d == playerTwoSymbol && f == playerTwoSymbol && e != playerOneSymbol))
+	{
+		row = 2;
+		return true;
+	}
+	if ((g == playerTwoSymbol && h == playerTwoSymbol && i != playerOneSymbol) || (h == playerTwoSymbol && i == playerTwoSymbol && g != playerOneSymbol) || (g == playerTwoSymbol && i == playerTwoSymbol && h != playerOneSymbol))
+	{
+		row = 3;
+		return true;
+	}
+	if ((a == playerTwoSymbol && d == playerTwoSymbol && g != playerOneSymbol) || (d == playerTwoSymbol && g == playerTwoSymbol && a != playerOneSymbol) || (a == playerTwoSymbol && g == playerTwoSymbol && d != playerOneSymbol))
+	{
+		column = 1;
+		return true;
+	}
+	if ((b == playerTwoSymbol && e == playerTwoSymbol && h != playerOneSymbol) || (e == playerTwoSymbol && h == playerTwoSymbol && b != playerOneSymbol) || (b == playerTwoSymbol && h == playerTwoSymbol && e != playerOneSymbol))
+	{
+		column = 2;
+		return true;
+	}
+	if ((c == playerTwoSymbol && f == playerTwoSymbol && i != playerOneSymbol) || (f == playerTwoSymbol && i == playerTwoSymbol && c != playerOneSymbol) || (c == playerTwoSymbol && i == playerTwoSymbol && f != playerOneSymbol))
+	{
+		column = 3;
+		return true;
+	}
+	if ((a == playerTwoSymbol && e == playerTwoSymbol && i != playerOneSymbol) || (e == playerTwoSymbol && i == playerTwoSymbol && a != playerOneSymbol) || (a == playerTwoSymbol && i == playerTwoSymbol && e != playerOneSymbol))
+	{
+		diagonal = 1;
+		return true;
+	}
+	if ((g == playerTwoSymbol && e == playerTwoSymbol && c != playerOneSymbol) || (e == playerTwoSymbol && c == playerTwoSymbol && g != playerOneSymbol) || (g == playerTwoSymbol && c == playerTwoSymbol && e != playerOneSymbol))
+	{
+		diagonal = 2;
+		return true;
+	}
+	return false;
+
+
+}
+
+bool terminat(int mat[3][3])
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			if (mat[i][j] == ' ')
+				return false;
+		}
+	}
+	return true;
+}
+
+int empty_c(int r)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		if (matrix[r][i] == ' ')
+			return i;
+	}
+}
+
+int empty_r(int c)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		if (matrix[i][c] == ' ')
+			return i;
+	}
+}
+
+void reset()
+{
+	row = 0;
+	column = 0;
+	diagonal = 0;
+}
